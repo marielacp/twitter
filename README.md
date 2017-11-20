5bd306e228aec73d30527dfdbb41222cdc0c4aca
marielacp <marielacp@outlook.com.pe>
2017-11-20 04:21:29 -0500

    añadiendo actualizaciones readme y js

---
+++ README.md
@@ -0,0 +1,44 @@
+---
+Title: "Creaciòn de replica de Pagina Twitter dinamica."
+Name: "Mariela Deysi Cerna Peralta"
+Fech: "20/11/2017"
+
+---
+
+ # En este reto, vamos a replicar el newsfeed de Twitter, para ello vamos a seguir un flujo de versiones que serviran de guia.
+
+ ## Enunciado.
+
+ ## Versión 0.0.1
+- Diseñar un formulario que permita ingresar un texto y un botón para "twittear".
+- Agregar un evento de click al botón o de submit al formulario.
+- En el evento, obtener el texto.
+- Agregar el texto al HTML.
+
+ ## Versión 0.0.2
+- No ingresar texto vacío (deshabilitar el botón de "twittear").
+- Contar la cantidad de caracteres de forma regresiva.
+
+ ## Versión 0.0.3
+- Si pasa los 140 caracteres, deshabilitar el botón.
+- Si pasa los 120 caracteres, mostrar el contador con OTRO color.
+- Si pasa los 130 caracteres, mostrar el contador con OTRO color.
+- Si pasa los 140 caracteres, mostrar el contador en negativo.
+
+ ## Versión 0.0.4
+- Al presionar enter(/n) que crezca el textarea de acuerdo al tamaño del texto.
+
+ ## Versión 0.0.5
+- Si la cantidad de caracteres ingresados (sin dar un enter), supera al tamaño del textarea por defecto, debe de agregarse una línea más para que no aparezca el scroll. (Si en caso aplica)
+
+ ## Versión 0.0.6 (Extra)
+- Agregar la hora en que se publicó el tweet. En el formato de 24 horas: hh:mm.
+
+***
+ ## Desarrollo
+1. Se crea la estructura de la página.
+2. Se aplica estilos.
+3. Se crea script.
+4. Se crea conexion de paginas.
+4. Configuracion de GitHubs.
+5. Creaciòn repositorio remoto y Pagina Web.
--- js/app.js
+++ js/app.js
@@ -1,78 +1,61 @@
-window.addEventListener('load',function(){
+window.addEventListener('load', function() {
+  var time = moment().format('LT');
+  var teclado = document.getElementById('txttarea');
+  var captura = document.getElementById('btn');
+  captura.addEventListener('click', enviotxt);
+  teclado.onkeyup = count;
+  teclado.onkeydown = count;
 
-var teclado = document.getElementById('txttarea');
-teclado.onkeyup=count;
-teclado.onkeydown=count;
 
-var captura = document.getElementById("btn");
-captura.addEventListener("click", enviotxt);
-
-function count() {
- var min = 45;
- var minn = 70;
- var pruebmin = 140; 
- var capacidad = 120;
- var capacidad1 = 130;
- var cont = document.forms[0].txttarea.value.length;
- var rest = pruebmin - cont;
- document.forms[0].caracteres.value = rest;
- if (cont <= min) {
-  teclado.style.height = "40px";
- } else if(cont<=minn) {  
-  teclado.style.height = "50px";
- } else {
-  teclado.style.height = "65px";
-};
-document.form.twiter.disabled=false
-var habilitado = document.getElementsByTagName('input')[0];
-habilitado.classList.remove('btnd');
-if(pruebmin<=cont){
-document.form.twiter.disabled = true
-habilitado.classList.add('btnd');
-};
-var inpcolor = document.getElementsByTagName('input')[1];
-if(cont>=capacidad && cont<capacidad1){
-inpcolor.style.color='red';
-} else if (cont >= capacidad1 && cont <= pruebmin) {
-  inpcolor.style.color = 'gray'; 
-}else{
-   inpcolor.style.color = 'blue'; 
-}
- };
-
-function enviotxt(){  
-  var d = new Date();
-  var tipo = "pm";
-  if (d.getHours<=1 && d.getHours<12){
-   tipo ="am";
-  }else {
-    tipo;
-  }
-  var container = document.getElementById('containermsn')
-   if (txttarea.value) {
+  function count() {
+    var habilitado = document.getElementsByTagName('input')[0];
+    var inpcolor = document.getElementsByTagName('input')[1];
+    var pruebmin = 140; 
+    var capacidad = 120;
+    var capacidad1 = 130;
+    var cont = document.forms[0].txttarea.value.length;
+    var rest = pruebmin - cont;
+    document.forms[0].caracteres.value = rest;
+    teclado.style.height = '0px';
+    teclado.style.height = (teclado.scrollHeight + 0) + 'px';
+    document.form.twiter.disabled = false;
+    habilitado.classList.remove('btnd');
+    if (pruebmin <= cont) {
+      document.form.twiter.disabled = true;
+      habilitado.classList.add('btnd');
+    }
+    if (cont >= capacidad && cont < capacidad1) {
+      inpcolor.style.color = 'red';
+    } else if (cont >= capacidad1 && cont <= pruebmin) {
+      inpcolor.style.color = 'orange'; 
+    } else {
+      inpcolor.style.color = 'blue'; 
+    }
+  };
+  function enviotxt() {  
+    var fecha = new Date();
+    var horas = fecha.getHours();
+    var ampm;
+    var minutos = fecha.getMinutes();
+    var container = document.getElementById('containermsn');
     var txt = document.createElement('textarea');
-    txt.classList.add('txt1');
-    txt.textContent = txttarea.value + '\n' + d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds() + "" + tipo;
-     container.appendChild(txt);
-     teclado.value = '';
-   };
- document.form.twiter.disabled = true;
- var habilitado = document.getElementsByTagName('input')[0];
- habilitado.classList.add('btnd');
-
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var habilitado = document.getElementsByTagName('input')[0];
+    if (horas >= 12) {
+      horas = horas - 12;
+      ampm = 'PM';
+    } else {
+      ampm = 'AM';
+    }
+    if (txttarea.value) {
+      txt.classList.add('txt1');
+      txt.textContent = txttarea.value + '\n' + fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds() + ' ' + ampm;
+      container.appendChild(txt);
+      teclado.value = '';
+      txt.style.height = '0px';
+      txt.style.height = (txt.scrollHeight + 0) + 'px';
+      txt.disabled = 'true';
+    };
+    document.form.twiter.disabled = true;
+    habilitado.classList.add('btnd');
+  };
 });
-  
